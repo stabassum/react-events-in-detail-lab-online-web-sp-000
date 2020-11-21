54d4194c116d4ffe2f42a0dfef1979975a9236e3
stabassum <sumu1993@gmail.com>
2020-11-21 11:57:46 -0500

    commit tree 94b9dc6df608b846d7ab26456a9e947fc64534ee

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-class CoordinatesButton extends Component{
+class CoordinatesButton extends React.Component{
 
     handleClick = event => {
         this.props.onReceiveCoordinates([event.clientX, event.clientY]);
